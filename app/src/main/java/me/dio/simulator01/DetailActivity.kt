55fb81b3d08c1bb6eef6ad5db81bc2cc03493445
Luana <luana.mouro@gmail.com>
2022-06-24 11:59:51 -0300

    Detalhes e main prontos

---
+++ app/src/main/java/me/dio/simulator01/DetailActivity.kt
@@ -0,0 +1,23 @@
+package me.dio.simulator01
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import me.dio.simulator01.databinding.ActivityDetailBinding
+import me.dio.simulator01.databinding.ActivityMainBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+    @SuppressLint("UseSupportActionBar")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
