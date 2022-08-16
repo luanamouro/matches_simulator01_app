d50238c04456a333222aab1a9512da209494b5fe
Luana <luana.mouro@gmail.com>
2022-08-16 15:25:35 -0300

    Com SetupMatchesList

--- app/src/main/java/me/dio/simulator01/R00/ui/MainActivity.java
+++ app/src/main/java/me/dio/simulator01/R00/ui/MainActivity.java
@@ -5,13 +5,22 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
+import me.dio.simulator01.R00.data.MatchesApi;
+import me.dio.simulator01.R00.domain.Match;
 import me.dio.simulator01.databinding.ActivityMainBinding;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private ActivityMainBinding binding;
+    private MatchesApi matchesApi;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,10 +38,25 @@ public class MainActivity extends AppCompatActivity {
     private void setupHttpClient() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://luanamouro.github.io/matches_simulator01_api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        matchesApi = retrofit.create(MatchesApi.class);
+
     }
 
     private void setupMatchesList() {
-        //TODO Listar as partidas (api).
+       matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
+           @Override
+           public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+
+           }
+
+           @Override
+           public void onFailure(Call<List<Match>> call, Throwable t) {
+
+           }
+       });
     }
 
     private void setupMatchesRefresh() {
