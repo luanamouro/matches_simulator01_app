830450c3f3cff936af3294b9cbbb88ef89488a0e
Luana <luana.mouro@gmail.com>
2022-08-16 23:15:02 -0300

    Final

--- app/src/main/java/me/dio/simulator01/R00/ui/DetailActivity.kt
+++ app/src/main/java/me/dio/simulator01/R00/ui/DetailActivity.kt
@@ -3,6 +3,8 @@ package me.dio.simulator01.R00.ui
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import me.dio.simulator01.R00.domain.Match
 import me.dio.simulator01.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -21,5 +23,30 @@ class DetailActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        loadMatchFromextra()
+    }
+
+    private fun loadMatchFromextra() {
+        intent?.extras?.getParcelable<Match>(Extras.MATCH)?.let {
+            Glide.with(this).load(it.place.image).into(binding.ivPlace)
+            supportActionBar?.title = it.place.name
+
+            binding.tvDescription.text = it.description
+
+            Glide.with(this).load(it.homeTeam.image).into(binding.ivHomeTeam01)
+            binding.tvHomeTeamName.text = it.homeTeam.name
+            binding.rbHomeTeamStars.rating = it.homeTeam.stars.toFloat()
+            if(it.homeTeam.score != null){
+                binding.tvHomeTeamScore.text = it.homeTeam.score.toString()
+            }
+
+            Glide.with(this).load(it.awayTeam.image).into(binding.ivAwayTeam01)
+            binding.tvAwayTeamName.text = it.awayTeam.name
+            binding.rbAwayTeamStars.rating = it.awayTeam.stars.toFloat()
+            if(it.awayTeam.score != null){
+                binding.tvAwayTeamScore.text = it.awayTeam.score.toString()
+            }
+        }
     }
 }
